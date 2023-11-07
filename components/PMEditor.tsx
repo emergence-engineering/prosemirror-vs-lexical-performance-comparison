23413fe9152a0c008d63cf3c41d7de91ae19f346
csillagk <katalin.csillag@emergence-engineering.com>
2023-11-07 07:20:22 +0100

    pm editor added

---
+++ components/PMEditor.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { Schema, DOMParser } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { exampleSetup } from "prosemirror-example-setup";
+import { useEffect } from "react";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks,
+});
+
+const PMEditor = () => {
+  useEffect(() => {
+    const view = new EditorView(
+      document.querySelector("#editor") as HTMLElement,
+      {
+        state: EditorState.create({
+          doc: DOMParser.fromSchema(mySchema).parse(
+            document.createElement("div"),
+          ),
+          plugins: exampleSetup({ schema: mySchema }),
+        }),
+      },
+    );
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      <div id={"editor"}></div>
+    </Wrapper>
+  );
+};
+
+export default PMEditor;
--- pages/lexical.tsx
+++ pages/lexical.tsx
@@ -1,13 +1,18 @@
+import Head from "next/head";
+import { Inter } from "@next/font/google";
 import Header from "../components/Header";
 import PMEditor from "../components/PMEditor";
 import Layout from "../components/Layout";
 import { NextPage } from "next";
+import LexicalEditor from "../components/LexicalEditor";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Home: NextPage = () => (
   <>
     <Layout>
       <Header />
-      <PMEditor />
+      <LexicalEditor />
     </Layout>
   </>
 );
