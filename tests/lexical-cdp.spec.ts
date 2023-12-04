7327055a0be69e77b6fb2fc7447baf463438203e
csillagk <katalin.csillag@emergence-engineering.com>
2023-12-04 18:56:47 +0100

    wip cdp

--- tests/lexical-cdp.spec.ts
+++ tests/lexical-cdp.spec.ts
@@ -6,9 +6,9 @@ import {
   Metric,
   relevantMetrics,
   selectText,
-  simulatePaste,
 } from "./utils";
 import fs from "fs";
+import path from "path";
 
 // find editor cdp
 // // for loop, average of metrics collected to a single file
@@ -22,20 +22,6 @@ import fs from "fs";
 // // y: bundle size, memory, cpu usage, latency
 // article
 
-// test.describe("CDP Demo", () => {
-//   test("Get performance metrics", async ({ page }) => {
-//     //Create a new connection to an existing CDP session to enable performance Metrics
-//     const session = await page.context().newCDPSession(page);
-//     //To tell the CDPsession to record performance metrics.
-//     await session.send("Performance.enable");
-//     await page.goto("http://localhost:3000/lexical");
-//     await page.waitForSelector(".ContentEditable__root");
-//     // await page.click(".ContentEditable__root");
-//     console.log("=============CDP Performance Metrics===============");
-//     let performanceMetrics = await session.send("Performance.getMetrics");
-//     console.log(performanceMetrics.metrics);
-//   });
-// });
 test.describe("Lexical - user interaction tests", () => {
   test("find editor", async ({ browser }) => {
     const perfArray: Metric[] = [];
@@ -58,12 +44,15 @@ test.describe("Lexical - user interaction tests", () => {
         },
       );
       perfArray.push(...perfMetricsFiltered);
+      if (i > 25) console.log("findEditor", i);
     }
 
     const averagedPerfMetrics = calcAverageMetrics(perfArray);
 
+    const folderPath = path.join(__dirname, "lexical-tests");
+
     fs.writeFile(
-      "findEditorL.json",
+      path.join(folderPath, "01findEditorL.json"),
       JSON.stringify(averagedPerfMetrics),
       "utf8",
       (err) => {
@@ -71,220 +60,157 @@ test.describe("Lexical - user interaction tests", () => {
           console.error(err);
           return;
         }
-        console.log("Lexical, result is in: findEditorL.json");
+        console.log("Lexical, result is in: 01findEditorL.json");
       },
     );
   });
 
-  test.only("input latency", async ({ page }) => {
-    test.setTimeout(70000);
-    await findEditor(page, "lexical", ".ContentEditable__root");
-    page;
+  test("input latency", async ({ browser }) => {
+    const perfArray = [];
 
-    const typeText = await page.evaluate(
-      async ([simulatePasteFunction]) => {
-        const perfTimes: number[] = [];
-        const chineseTextTimes: number[] = [];
-        const editor = document.querySelector(
-          ".ContentEditable__root",
-        ) as HTMLElement | null;
-        if (!editor) return { perfTimes, chineseTextTimes };
-        const simulatePasteFn = new Function(
-          "return " + simulatePasteFunction,
-        )();
-        const undoButton = Array.from(
-          document.querySelectorAll("button.toolbar__item"),
-        ).find((button) => button.textContent === "undo") as HTMLElement | null;
-        if (!undoButton) return { perfTimes, chineseTextTimes };
+    for (let i = 0; i < 30; i++) {
+      const page = await browser.newPage();
+      const session = await page.context().newCDPSession(page);
+      await session.send("Performance.enable");
+      await findEditor(page, "lexical", ".ContentEditable__root");
 
-        const textChars = "Typing".split("");
-        const chineseTextChars = "那不是杂志是".split("");
+      await page.evaluate(async () => {
+        document.execCommand("insertText", false, "Typing");
+      });
 
-        for (let i = 0; i < 1000; i++) {
-          performance.mark("start");
-          // couldn't find a better way to 'type'
-          for (const char of textChars) {
-            await simulatePasteFn(char, editor);
-          }
-          performance.mark("end");
-          await undoButton.click();
+      const performanceMetrics = await session.send("Performance.getMetrics");
+      await session.detach();
+      await page.close();
+
+      const perfMetricsFiltered = performanceMetrics.metrics.filter(
+        (metric) => {
+          return relevantMetrics.includes(metric.name);
+        },
+      );
+      perfArray.push(...perfMetricsFiltered);
+      if (i > 25) console.log("typing", i + 1);
+    }
+
+    const averagedPerfMetrics = calcAverageMetrics(perfArray);
+
+    const folderPath = path.join(__dirname, "lexical-tests");
+
+    fs.writeFile(
+      path.join(folderPath, "02typingL.json"),
+      JSON.stringify(averagedPerfMetrics),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-
-        for (let j = 0; j < 1000; j++) {
-          performance.mark("start-chinese");
-          for (const char of chineseTextChars) {
-            await simulatePasteFn(char, editor);
-          }
-          performance.mark("end-chinese");
-          await undoButton.click();
-        }
-
-        performance.measure("typing", "start", "end");
-        const latency = performance.getEntriesByName("typing").pop();
-        if (!latency) return { perfTimes, chineseTextTimes };
-        perfTimes.push(latency.duration);
-
-        performance.measure("typing-chinese", "start-chinese", "end-chinese");
-        const latencyChinese = performance
-          .getEntriesByName("typing-chinese")
-          .pop();
-        if (!latencyChinese) return { perfTimes, chineseTextTimes };
-        chineseTextTimes.push(latencyChinese.duration);
-
-        return { perfTimes, chineseTextTimes };
+        console.log("Lexical, result is in: 02typingL.json");
       },
-      [simulatePaste.toString()],
-    );
-    const { perfTimes, chineseTextTimes } = typeText;
-    console.log(`Typing Performance: ${averageOf(perfTimes)}ms`);
-    console.log(
-      `Chinese text Typing Performance: ${averageOf(chineseTextTimes)}ms`,
     );
   });
 
-  test("paste operation performance", async ({ page }) => {
-    const pastePerf = await page.evaluate(
-      async ([simulatePasteFunction]) => {
-        const perfTimes: number[] = [];
-        const perfTimesChinese: number[] = [];
-        const hrTimes: number[] = [];
+  test("paste operation performance", async ({ browser }) => {
+    const perfArray = [];
 
-        const editor = document.querySelector(
-          ".ContentEditable__root",
-        ) as HTMLElement | null;
-        if (!editor) return { perfTimes, perfTimesChinese, hrTimes };
-        const undoButton = Array.from(
-          document.querySelectorAll("button.toolbar__item"),
-        ).find((button) => button.textContent === "undo") as HTMLElement | null;
-        const hrButton = Array.from(
-          document.querySelectorAll("button.toolbar__item"),
-        ).find((button) => button.textContent === "HR") as HTMLElement | null;
-        if (!undoButton || !hrButton)
-          return { perfTimes, perfTimesChinese, hrTimes };
+    for (let i = 0; i < 30; i++) {
+      const page = await browser.newPage();
+      const session = await page.context().newCDPSession(page);
+      await session.send("Performance.enable");
+      await findEditor(page, "lexical", ".ContentEditable__root");
 
-        const simulatePasteFn = new Function(
-          "return " + simulatePasteFunction,
-        )();
+      await page.evaluate(() => {
+        document.execCommand("insertText", false, "typing and ".repeat(1000));
+      });
 
-        for (let i = 0; i < 1000; i++) {
-          performance.mark("start");
-          await simulatePasteFn(
-            "This is just a sentence pasted 1000 times. ".repeat(1000),
-            editor,
-          );
-          performance.mark("end");
-          await undoButton.click();
+      const performanceMetrics = await session.send("Performance.getMetrics");
+      await session.detach();
+      await page.close();
 
-          performance.measure("paste", "start", "end");
-          const pasteLatency = performance.getEntriesByName("paste").pop();
-          if (!pasteLatency) return { perfTimes, perfTimesChinese, hrTimes };
-          perfTimes.push(pasteLatency.duration);
+      const perfMetricsFiltered = performanceMetrics.metrics.filter(
+        (metric) => {
+          return relevantMetrics.includes(metric.name);
+        },
+      );
+      perfArray.push(...perfMetricsFiltered);
+      if (i > 25) console.log("typing", i + 1);
+    }
+
+    const averagedPerfMetrics = calcAverageMetrics(perfArray);
+
+    const folderPath = path.join(__dirname, "lexical-tests");
+    fs.writeFile(
+      path.join(folderPath, "03pasteL.json"),
+      JSON.stringify(averagedPerfMetrics),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-
-        for (let i = 0; i < 1000; i++) {
-          performance.mark("start-chinese");
-          await simulatePasteFn(
-            "李红：不，那不是杂志。那是字典。".repeat(1000),
-            editor,
-          );
-          performance.mark("end-chinese");
-          await undoButton.click();
-
-          performance.measure("paste-chinese", "start-chinese", "end-chinese");
-          const pasteChineseLatency = performance
-            .getEntriesByName("paste-chinese")
-            .pop();
-          if (!pasteChineseLatency)
-            return { perfTimes, perfTimesChinese, hrTimes };
-          perfTimesChinese.push(pasteChineseLatency.duration);
-        }
-
-        for (let i = 0; i < 1000; i++) {
-          performance.mark("start-hr");
-          await hrButton.click();
-          performance.mark("end-hr");
-          await undoButton.click();
-
-          performance.measure("paste-hr", "start-hr", "end-hr");
-          const pasteHRLatency = performance.getEntriesByName("paste-hr").pop();
-          if (!pasteHRLatency) return { perfTimes, perfTimesChinese, hrTimes };
-          hrTimes.push(pasteHRLatency.duration);
-        }
-
-        return { perfTimes, perfTimesChinese, hrTimes };
+        console.log("Lexical, result is in: 03pasteL.json");
       },
-      [simulatePaste.toString()],
     );
-    const { perfTimes, perfTimesChinese, hrTimes } = pastePerf;
-    console.log(`Average Paste Operation Time: ${averageOf(perfTimes)}ms`);
-    console.log(
-      `Average Chinese Text Paste Operation Time: ${averageOf(
-        perfTimesChinese,
-      )}ms`,
-    );
-    console.log(`Average HR Paste Operation Time: ${averageOf(hrTimes)}ms`);
   });
 
-  test("bold formatting text performance", async ({ page, browser }) => {
-    await page.keyboard.insertText(
-      "This is a test for formatting large text. ".repeat(1000),
-    );
+  test.only("bold formatting text performance", async ({ browser }) => {
+    const perfArray = [];
 
-    const formatTextDuration = await page.evaluate(
-      async ([selectTextFunction]) => {
-        const perfTimes: number[] = [];
-        const removingTimes: number[] = [];
+    for (let i = 0; i < 30; i++) {
+      const page = await browser.newPage();
+      const session = await page.context().newCDPSession(page);
+      await session.send("Performance.enable");
+      await findEditor(page, "lexical", ".ContentEditable__root");
+      await page.keyboard.insertText("formatted text and ".repeat(1000));
+
+      await page.evaluate(async () => {
         const editor = document.querySelector(
           ".ContentEditable__root",
         ) as HTMLElement | null;
-        if (!editor) return { perfTimes, removingTimes };
         const boldButton = Array.from(
           document.querySelectorAll("button.toolbar__item"),
         ).find((button) => button.textContent === "B") as HTMLElement | null;
-        if (!boldButton) return { perfTimes, removingTimes };
-        const selectTextFn = new Function("return " + selectTextFunction)();
+        if (!boldButton || !editor) return;
 
-        selectTextFn(editor);
+        const isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
+        const selectEvent = new KeyboardEvent("keydown", {
+          key: "A",
+          keyCode: 65, // keyCode for Backspace
+          code: "KeyA",
+          ctrlKey: !isMac,
+          metaKey: isMac, // Cmd key on Mac
+          bubbles: true,
+        });
 
-        for (let i = 0; i < 1000; i++) {
-          performance.mark("start-formatting");
-          await boldButton.click();
-          performance.mark("end-formatting");
+        editor.dispatchEvent(selectEvent);
+        await boldButton.click();
+      });
 
-          performance.mark("start-removing");
-          await boldButton.click();
-          performance.mark("end-removing");
+      const performanceMetrics = await session.send("Performance.getMetrics");
+      await session.detach();
+      await page.close();
 
-          performance.measure(
-            "text-formatting",
-            "start-formatting",
-            "end-formatting",
-          );
-          const measure = performance.getEntriesByName("text-formatting").pop();
-          if (!measure) return { perfTimes, removingTimes };
-          perfTimes.push(measure.duration);
+      const perfMetricsFiltered = performanceMetrics.metrics.filter(
+        (metric) => {
+          return relevantMetrics.includes(metric.name);
+        },
+      );
+      perfArray.push(...perfMetricsFiltered);
+      if (i > 25) console.log("bold", i + 1);
+    }
+    const averagedPerfMetrics = calcAverageMetrics(perfArray);
 
-          performance.measure(
-            "remove-formatting",
-            "start-removing",
-            "end-removing",
-          );
-          const measureRemoving = performance
-            .getEntriesByName("remove-formatting")
-            .pop();
-          if (!measureRemoving) return { perfTimes, removingTimes };
-          removingTimes.push(measureRemoving.duration);
+    const folderPath = path.join(__dirname, "lexical-tests");
+    fs.writeFile(
+      path.join(folderPath, "04formattingL.json"),
+      JSON.stringify(averagedPerfMetrics),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-
-        return { perfTimes, removingTimes };
+        console.log("Lexical, result is in: 04formattingL.json");
       },
-      [selectText.toString()],
-    );
-
-    const { perfTimes, removingTimes } = formatTextDuration;
-    console.log(`Average Text Formatting Duration: ${averageOf(perfTimes)}ms`);
-    console.log(
-      `Average time of removing Text Formatting: ${averageOf(removingTimes)}ms`,
     );
   });
 
